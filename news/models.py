87c0b5fe7c855d99d346a5556c2aed2ecdf332d0
IreriVikki <wambsviki@gmail.com>
2018-09-24 17:32:43 +0300

    added a test file to test whether my project is working successfully

--- news/models.py
+++ news/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Editor(models.Model):
+    # charfields are like the equivalent of string field in flask
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name']
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    # i think this mf right here will add the current date and time of Article creation automatically by default
+    pub_date = models.DateTimeField(auto_now_add=True)
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Editor
 
 # Create your tests here.
+
+
+class EditorTestClass(TestCase):
+
+    # setup function
+    def setUp(self):
+        self.victor = Editor(first_name='Victor',
+                             last_name='Ireri', email='vikki@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.victor, Editor))
